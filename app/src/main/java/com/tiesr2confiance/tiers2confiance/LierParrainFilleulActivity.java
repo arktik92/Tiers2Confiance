fd3b6b189ea8a718fdf086126981a71d5de30719
camille <lesaichot.camille@gmail.com>
2021-11-22 14:48:53 +0100

    Ajout de l'empty activity LierParrainFilleul

---
+++ app/src/main/java/com/tiesr2confiance/tiers2confiance/LierParrainFilleulActivity.java
@@ -0,0 +1,14 @@
+package com.tiesr2confiance.tiers2confiance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LierParrainFilleulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lier_parrain_filleul);
+    }
+}
