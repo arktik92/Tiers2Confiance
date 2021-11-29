fcbb5fdf24baa4e1c8a0ec2f67627de92ac34c12
esteban <esteban.semellier@gmail.com>
2021-11-29 12:15:48 +0100

    ajout picker date et nettoyage du code

--- app/src/main/java/com/tiesr2confiance/tiers2confiance/Login/CreationProfilActivity.java
+++ app/src/main/java/com/tiesr2confiance/tiers2confiance/Login/CreationProfilActivity.java
@@ -1,9 +1,14 @@
 package com.tiesr2confiance.tiers2confiance.Login;
 
+import static android.graphics.Color.TRANSPARENT;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.ColorStateListDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,10 +28,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.type.Color;
 import com.google.type.DateTime;
 import com.tiesr2confiance.tiers2confiance.R;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -42,14 +49,12 @@ public class CreationProfilActivity extends AppCompatActivity {
     private static final String TAG = "CreationProfilActivity";
     private EditText etLastName, etFistName, etNickName, etZipCode, etCity;
     private TextView tvDateOfBirth;
-    private String lastName,firstName,nickName, dateOfBirth,zipCode,city, userId, userEmail, nephewsRequestTo, nephewsRequestfrom, nephews, godfatherRequestTo, godfatherRequestFrom, godfather, image;
+    private String lastName,firstName,nickName, dateOfBirth, zipCode,city, userId, userEmail, nephewsRequestTo, nephewsRequestfrom, nephews, godfatherRequestTo, godfatherRequestFrom, godfather, image;
     private long role;
     private RadioGroup rgRadioGroup;
     private RadioButton rbHomme, rbFemme;
-    private FrameLayout datePicker;
-    private Timestamp currentDate, registeredDate;
-
-
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private Timestamp currentDate, registeredDate, timestamp;
 
     /** Variable Firebase Auth **/
     FirebaseUser user;
@@ -69,7 +74,8 @@ public class CreationProfilActivity extends AppCompatActivity {
         rgRadioGroup =findViewById(R.id.rg_radio_group);
         rbHomme = findViewById(R.id.rb_homme);
         rbFemme = findViewById(R.id.rb_femme);
-        datePicker = findViewById(R.id.picker);
+        tvDateOfBirth = findViewById(R.id.tv_date_of_birth);
+
 
         role = 1;
 
@@ -80,24 +86,54 @@ public class CreationProfilActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation_profil);
 
-    init();
+        init();
 
+        /** Méthode OnClickListener du Date Picker **/
+        tvDateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal;
+                cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
 
+                DatePickerDialog dialog = new DatePickerDialog(CreationProfilActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        (DatePickerDialog.OnDateSetListener) mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(TRANSPARENT));
+                dialog.show();
+            }
+        });
 
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                 dateOfBirth = dayOfMonth + "-" + month + "-" + year;
+                tvDateOfBirth.setText(dateOfBirth);
+                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                try {
+                    Date date = format.parse(dateOfBirth);
+                    timestamp = new Timestamp(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void creationUser(View view) {
-
-
-
         userEmail = user.getEmail();
-        //dateOfBirth = etDateOfBirth.getText().toString().trim();
         city = etCity.getText().toString().trim();
         firstName = etFistName.getText().toString().trim();
         lastName = etLastName.getText().toString().trim();
@@ -120,7 +156,7 @@ public class CreationProfilActivity extends AppCompatActivity {
         Map<String, Object> userList = new HashMap<>();
         userList.put("us_auth_uid", userId);
         userList.put("us_email", userEmail);
-        userList.put("us_birth_date", dateOfBirth);
+        userList.put("us_birth_date", timestamp);
         userList.put("us_city", city);
         userList.put("us_first_name", firstName);
         userList.put("us_last_name", lastName);
@@ -151,10 +187,5 @@ public class CreationProfilActivity extends AppCompatActivity {
                         Log.e(TAG, "onFailure: ", e );
                     }
                 });
-
-    }
-    /** Methode onClick datePicker **/
-    public void onClickEtDateOfBirth(View view) {
-        datePicker.setVisibility(View.VISIBLE);
     }
 }
