64e5f2b79e6ed02c7d8cb2019d0b9ed67e7c81e5
Philippe GONCALVES <iguazu75greta@gmail.com>
2021-11-30 17:04:39 +0100

    Creation du code de gestion des RadioBoutons en Dynamique à partir des données Firestrore. >> Genders >> Hobbies
    Creation de la classe GlobalClass pour la gestion des Variables Globales :
        private FirebaseFirestore db;
        private FirebaseUser user;
        private String userId;
        private String userCountryLanguage;
        private String userNickName;
        private String userEmail;
    
        private  ArrayList<ModelGenders> arrayListGenders = new ArrayList<>();
    
    ##### Attention ######
    La méthode associée à
        private ArrayList<ModelHobbies> arrayListHobbies = new ArrayList<>();
    
    n'a pas encore été créé.

---
+++ app/src/main/java/com/tiesr2confiance/tiers2confiance/Common/GlobalClass.java
@@ -0,0 +1,195 @@
+package com.tiesr2confiance.tiers2confiance.Common;
+
+import android.app.Application;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.tiesr2confiance.tiers2confiance.Models.ModelGenders;
+import com.tiesr2confiance.tiers2confiance.Models.ModelHobbies;
+
+import java.util.ArrayList;
+
+public class GlobalClass extends Application {
+    private static final String TAG = "LOGAPP_GlobalClass";
+
+    private FirebaseFirestore db;
+    private FirebaseUser user;
+    private String userId;
+    private String userCountryLanguage;
+    private String userNickName;
+    private String userEmail;
+
+    private  ArrayList<ModelGenders> arrayListGenders = new ArrayList<>();
+    private ArrayList<ModelHobbies> arrayListHobbies = new ArrayList<>();
+
+    /************************* Constructors     ***************/
+    public GlobalClass() {
+
+    }
+
+    public GlobalClass(FirebaseFirestore db, FirebaseUser user, String userId, String userCountryLanguage, String userNickName, String userEmail, ArrayList<ModelGenders> arrayListGenders, ArrayList<ModelHobbies> arrayListHobbies) {
+        this.db = db;
+        this.user = user;
+        this.userId = userId;
+        this.userCountryLanguage = userCountryLanguage;
+        this.userNickName = userNickName;
+        this.userEmail = userEmail;
+        this.arrayListGenders = arrayListGenders;
+        this.arrayListHobbies = arrayListHobbies;
+    }
+
+    /************************* Setters     ***************/
+
+    public void setDb(FirebaseFirestore db) {
+        this.db = db;
+    }
+
+    public void setUser(FirebaseUser user) {
+        this.user = user;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUserCountryLanguage(String userCountryLanguage) {
+        this.userCountryLanguage = userCountryLanguage;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setArrayListGenders(ArrayList<ModelGenders> arrayListGenders) {
+        this.arrayListGenders = arrayListGenders;
+    }
+
+    public void setArrayListHobbies(ArrayList<ModelHobbies> arrayListHobbies) {
+        this.arrayListHobbies = arrayListHobbies;
+    }
+
+    /************************* Getters     ***************/
+
+    public FirebaseFirestore getDb() {
+        return db;
+    }
+
+    public FirebaseUser getUser() {
+        return user;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserCountryLanguage() {
+        return userCountryLanguage;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public ArrayList<ModelGenders> getArrayListGenders() {
+        return arrayListGenders;
+    }
+
+    public ArrayList<ModelHobbies> getArrayListHobbies() {
+        return arrayListHobbies;
+    }
+
+    /************************* Loaders     ***************/
+
+    public void loadGendersDataFromFirestore() {
+
+        Log.i(TAG, "loadGendersDataFromFirestore: BEGIN");
+        ArrayList<ModelGenders> myArrayListGenders = new ArrayList<>();
+        Log.i(TAG, "loadGendersDataFromFirestore: BEGIN userCountryLanguage >>>>" + userCountryLanguage);
+
+         myArrayListGenders.clear();
+
+//        user = FirebaseAuth.getInstance().getCurrentUser();
+//        userId = user.getUid();
+//        db = FirebaseFirestore.getInstance();
+        Query queryGenders = db.collection("genders")
+                .whereEqualTo("ge_country", userCountryLanguage);
+        //.whereNotIn("us_auth_uid", critere) ici ça plante ??
+        //.orderBy("ge_label");
+        //.orderBy("ge_label", Query.Direction.DESCENDING);
+        //.orderBy("ge_label", Query.Direction.ASCENDING);
+
+        queryGenders.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot documentSnapshots) {
+                if (documentSnapshots.isEmpty()) {
+                    Log.i(TAG, "onSuccess: LIST EMPTY");
+
+                } else {
+                    for (DocumentSnapshot documentSnapshot : documentSnapshots) {
+                        if (documentSnapshot.exists()) {
+                            String genderDocId = documentSnapshot.getId();
+
+                            Log.i(TAG, "onSuccess: DOCUMENT => " + documentSnapshot.getId() + " ; " + documentSnapshot.getData());
+//                            DocumentReference documentReference1 = FirebaseFirestore.getInstance().document("some path");
+                            DocumentReference docRefGender = db.document("genders/"+ genderDocId);
+                            docRefGender.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                @Override
+                                public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                                    ModelGenders gender= documentSnapshot.toObject(ModelGenders.class);
+                                    Log.i(TAG, "onSuccess ******** gender : " + gender.getGe_id() + " " + gender.getGe_country() + " " + gender.getGe_label());
+                                    myArrayListGenders.add(gender);
+                                    Log.i(TAG, "onSuccess XXXXX VarGlobale XXXXXX////// mArrayListGenders : " + myArrayListGenders);
+                                    Log.i(TAG, "XXXXXX VarGlobale XXXXX////// myArrayListGenders.size() *********** " + myArrayListGenders.size());
+
+                                }
+
+
+                            });
+                        }
+                    }
+
+
+
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error getting genders from FireStore!!!", Toast.LENGTH_LONG).show();
+                        Log.e(TAG, "onFailure : Error getting genders from FireStore!!!");
+
+                    }
+                });
+
+
+        Log.i(TAG, "onSuccess **** VarGlobal e**** mArrayListGenders : " + myArrayListGenders);
+        Log.i(TAG, "**** VarGlobale ***** myArrayListGenders.size() *********** " + myArrayListGenders.size());
+
+        arrayListGenders = myArrayListGenders;
+
+        Log.i(TAG, "loadGendersDataFromFirestore: END");
+    } // END loadGendersDataFromFirestore()
+}
--- app/src/main/java/com/tiesr2confiance/tiers2confiance/MainActivity.java
+++ app/src/main/java/com/tiesr2confiance/tiers2confiance/MainActivity.java
@@ -4,23 +4,129 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.tiesr2confiance.tiers2confiance.Common.GlobalClass;
+import com.tiesr2confiance.tiers2confiance.Common.Util;
 import com.tiesr2confiance.tiers2confiance.LierParrainFilleul.LierParrainFilleulActivity;
 import com.tiesr2confiance.tiers2confiance.LierParrainFilleul.PendingRequestsActivity;
+import com.tiesr2confiance.tiers2confiance.Models.ModelUsers;
 import com.tiesr2confiance.tiers2confiance.User.UserActivity;
 
 public class MainActivity extends AppCompatActivity {
 
 
     /**  Var globales **/
+    private static final String TAG = "LOGAPP_MainActivity";
+
+    /** Variable Firebase Auth **/
+    private FirebaseFirestore db;
+    private static FirebaseUser user;
+    private static String userId;
+    private static String userNickName;
+    private static String userCountryLanguage = "";
+    private static String userEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initGlobalVariables();
+
+        //initArrays();
+
+        Log.i(TAG, "------- END OF onCreate ---------");
     }
+
+    private void initArrays() {
+        final GlobalClass globalVariables = (GlobalClass) getApplicationContext();
+        globalVariables.loadGendersDataFromFirestore();
+
+
+
+        Log.i(TAG, "------- END OF initGlobalVariables ---------");
+    }
+
+    public void initGlobalVariables(){
+        final GlobalClass globalVariables = (GlobalClass) getApplicationContext();
+
+
+        db      = FirebaseFirestore.getInstance();
+        user    = FirebaseAuth.getInstance().getCurrentUser();
+        userId  = user.getUid();
+
+        globalVariables.setDb(db);
+        globalVariables.setUser(user);
+        globalVariables.setUserId(userId);
+
+        getUserDataFromFirestore(); // userNickName, userCountryLanguage
+
+
+        globalVariables.setUserNickName(userNickName);
+        globalVariables.setUserCountryLanguage(userCountryLanguage);
+        globalVariables.setUserEmail(userEmail);
+
+        Log.i(TAG, "userId : " + userId);
+        Log.i(TAG, "userNickName : " + userNickName);
+        Log.i(TAG, "userCountryLanguage : " + userCountryLanguage);
+        Log.i(TAG, "userEmail : " + userEmail);
+        Log.i(TAG, "------- END OF initGlobalVariables ---------");
+    }
+
+
+    public void getUserDataFromFirestore() {
+        final GlobalClass globalVariables = (GlobalClass) getApplicationContext();
+        DocumentReference docRefUserConnected;
+
+//        docRefUserConnected = db.document("users/"+ userId); //bSfRUKasZ7PyHnew1jwqG6jksl03
+        docRefUserConnected = db.document("users/bSfRUKasZ7PyHnew1jwqG6jksl03"); //
+        docRefUserConnected.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot myDocSnapshot) {
+
+                if (myDocSnapshot.exists()) {
+
+                    userNickName    = myDocSnapshot.getString("us_nickname");// + " " + userId;
+                    userEmail       = myDocSnapshot.getString("us_email");// + " " + userId;
+
+                    ModelUsers connectedUser = myDocSnapshot.toObject(ModelUsers.class);
+                    userCountryLanguage     =   connectedUser.getUs_country_lang().toString().trim();
+
+
+//                    Mise à jour des variables ici car délai de latence dans l'init des variables.
+                    globalVariables.setUserNickName(userNickName);
+                    globalVariables.setUserCountryLanguage(userCountryLanguage);
+                    globalVariables.setUserEmail(userEmail);
+
+                    Log.i(TAG, "userId : " + userId);
+                    Log.i(TAG, "userNickName : " + userNickName);
+                    Log.i(TAG, "userCountryLanguage : " + userCountryLanguage);
+                    Log.i(TAG, "userEmail : " + userEmail);
+
+                    Log.i(TAG, "----- Update on getUserDataFromFirestore -----");
+
+                    Toast.makeText(MainActivity.this,"USER FOUND in collection \"users\"", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    userNickName = "No USER FOUND";
+                    Toast.makeText(MainActivity.this,"No Document found in collection \"users\" for this LOGIN/USER", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Log.i(TAG, "----- END getUserDataFromFirestore -----");
+    } // END getUserDataFromFirestore()
 
     public void launchLierParrainFilleulActivity(View v){
         Intent intent = new Intent(MainActivity.this, LierParrainFilleulActivity.class);
@@ -41,6 +147,10 @@ public class MainActivity extends AppCompatActivity {
     public void launchUserActivity(View v){
         Intent intent = new Intent(MainActivity.this, UserActivity.class);
 //        intent.putExtra("IdUser", snapshot.getId());
+
+        final GlobalClass globalVariables = (GlobalClass) getApplicationContext();
+        globalVariables.loadGendersDataFromFirestore();
+
         startActivity(intent);
 
 
