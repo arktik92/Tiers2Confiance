c2fb13a194126560afaeda018a5b3d3b619ad774
camille <lesaichot.camille@gmail.com>
2021-12-03 11:37:04 +0100

    Simplification de la liste de demande de parrain

--- app/src/main/java/com/tiesr2confiance/tiers2confiance/LierParrainFilleul/LierParrainFilleulAdapter.java
+++ app/src/main/java/com/tiesr2confiance/tiers2confiance/LierParrainFilleul/LierParrainFilleulAdapter.java
@@ -19,8 +19,10 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -36,6 +38,7 @@ import java.util.Date;
 public class LierParrainFilleulAdapter extends FirestoreRecyclerAdapter<ModelUsers, LierParrainFilleulAdapter.ItemViewHolder> {
 
     private static final String TAG = "Lier P/F ADAPTER :";
+    private static final String TAGAPP = "LOGAPP";
     // Le context
     private Context context;
     // Liste basée sur le model ModelUser
@@ -94,8 +97,8 @@ public class LierParrainFilleulAdapter extends FirestoreRecyclerAdapter<ModelUse
         String us_nephews_request_from = model.getUs_nephews_request_from();
         String us_avatar = model.getUs_avatar();
 
-        holder.tv_nickname.setText(us_nickname);
-        holder.tv_city.setText(us_city);
+        holder.tvNickname.setText(us_nickname);
+        holder.tvCity.setText(us_city);
 
         //String str = String.format("%tc", us_birth_day);
 
@@ -108,7 +111,7 @@ public class LierParrainFilleulAdapter extends FirestoreRecyclerAdapter<ModelUse
                 .error(R.mipmap.ic_launcher)
                 .placeholder(R.mipmap.ic_launcher);
 
-        Context context = holder.iv_photo_profil.getContext();
+        Context context = holder.ivPhotoProfil.getContext();
         // Fonctionne soit avec un contexte ou une vue (with), puis avec un load d'url,
         // puis le chargement des options précédemment crées, puis centrer l'image,
         // puis diskCacheStrategy
@@ -118,62 +121,66 @@ public class LierParrainFilleulAdapter extends FirestoreRecyclerAdapter<ModelUse
                 .apply(options)
                 .fitCenter()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(holder.iv_photo_profil);
+                .into(holder.ivPhotoProfil);
 
         // Association du listener au bouton Envoyer la demande (de parrainage, de filleul)
-        holder.btn_request.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userPosition = usersCollectionRef.document(getSnapshots().getSnapshot(holder.getAbsoluteAdapterPosition()).getId());
-                userConnected.get()
-                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                            @Override
-                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                ModelUsers contenuUser = documentSnapshot.toObject(ModelUsers.class);
-                                assert contenuUser != null;
-                                // Si l'utilisateur connecté est un célibataire, il cherche et demande à des parrains d'être leur filleul
-                                if (contenuUser.getUs_role() == 1) {
-                                    Log.e(TAG , "je suis un célibataire connecté, j'envoi une demande à un parrain");
-                                    userConnected.update("us_godfather_request_to", contenuUser.getUs_godfather_request_to() + userPosition.getId()+  ";" );
-                                    userPosition.update("us_nephews_request_from",   us_nephews_request_from + userConnected.getId()+  ";");
-                                    Log.e(TAG, "Demande à un parrain envoyé");
-                                }
-
-                                // Si l'utilisateur connecté est un parrain, il cherche et demande à des célibataires de les parrainer
-                                if (contenuUser.getUs_role() == 2) {
-                                    Log.e(TAG , "je suis un parrain connecté, j'envoie une demande à un célibataire");
-                                    userConnected.update("us_nephews_request_to", contenuUser.getUs_nephews_request_to() + userPosition.getId()+  ";");
-                                    userPosition.update("us_godfather_request_from", us_godfather_request_from + userConnected.getId()+  ";");
-                                    Log.e(TAG, "Demande à un célibataire envoyé");
-                                }
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.e(TAG, "erreur" + currentUser.getUid());
-                            }
-                        });
-                holder.btn_request.setText("Demande envoyée");
-                holder.btn_request.setEnabled(false);
-            }
-        });
+//        holder.btnRequest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                userPosition = usersCollectionRef.document(getSnapshots().getSnapshot(holder.getAbsoluteAdapterPosition()).getId());
+//                userConnected.get()
+//                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                                Log.i(TAGAPP, "****************  onBindViewHolder addOnSuccessListener  onSuccess  ****");
+//
+//                                ModelUsers contenuUser = task.getResult().toObject(ModelUsers.class);
+//                                // Si l'utilisateur connecté est un célibataire, il cherche et demande à des parrains d'être leur filleul
+//                                if (contenuUser.getUs_role() == 1) {
+//                                    Log.e(TAG, "je suis un célibataire connecté, j'envoi une demande à un parrain");
+//                                    userConnected.update("us_godfather_request_to", contenuUser.getUs_godfather_request_to() + userPosition.getId() + ";");
+//                                    userPosition.update("us_nephews_request_from", us_nephews_request_from + userConnected.getId() + ";");
+//                                    Log.e(TAG, "Demande à un parrain envoyé");
+//                                }
+//
+//                                // Si l'utilisateur connecté est un parrain, il cherche et demande à des célibataires de les parrainer
+//                                if (contenuUser.getUs_role() == 2) {
+//                                    Log.e(TAG, "je suis un parrain connecté, j'envoie une demande à un célibataire");
+//                                    userConnected.update("us_nephews_request_to", contenuUser.getUs_nephews_request_to() + userPosition.getId() + ";");
+//                                    userPosition.update("us_godfather_request_from", us_godfather_request_from + userConnected.getId() + ";");
+//                                    Log.e(TAG, "Demande à un célibataire envoyé");
+//                                }
+//
+//                            }
+//                        })
+//
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.e(TAG, "erreur" + currentUser.getUid());
+//                            }
+//                        });
+//
+//
+//                holder.btnRequest.setText("Demande envoyée");
+//                holder.btnRequest.setEnabled(false);
+//
+//            }
+//        });
     }
 
     public class ItemViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView tv_nickname, tv_city;
-        public ImageView iv_photo_profil;
-        public Button btn_request;
-        public CardView cv_profil_user;
+        public TextView tvNickname, tvCity;
+        public ImageView ivPhotoProfil;
+        public CardView cvProfilUser;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
-            tv_nickname = itemView.findViewById(R.id.tv_nickname);
-            tv_city = itemView.findViewById(R.id.tv_city);
-            iv_photo_profil = itemView.findViewById(R.id.iv_photo_profil);
-            btn_request = itemView.findViewById(R.id.btn_request);
-            cv_profil_user = itemView.findViewById(R.id.cv_profil_user);
+            tvNickname = itemView.findViewById(R.id.tv_nickname);
+            tvCity = itemView.findViewById(R.id.tv_city);
+            ivPhotoProfil = itemView.findViewById(R.id.iv_photo_profil);
+            cvProfilUser = itemView.findViewById(R.id.cv_profil_user);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
