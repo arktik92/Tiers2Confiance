fe587b932f659f951e556c4a628ecfec3cb994cb
Philippe GONCALVES <iguazu75greta@gmail.com>
2021-12-07 17:13:55 +0100

    Merge Mardi 7

--- app/src/main/java/com/tiesr2confiance/tiers2confiance/Common/NodesNames.java
+++ app/src/main/java/com/tiesr2confiance/tiers2confiance/Common/NodesNames.java
@@ -4,22 +4,32 @@ import java.util.Date;
 
 public class NodesNames {
 
+
     // Node des Users
     public static final String KEY_ROLE = "us_role";
     public static final String KEY_NICKNAME = "us_nickname";
     public static final String KEY_NAME = "us_first_name";
     public static final String KEY_LAST_NAME = "us_last_name";
-    public static final String KEY_IMG = "us_img";
+    public static final String KEY_PHOTOS = "us_photos";
     public static final String KEY_CODE_POSTAL = "us_postal_code";
     public static final String KEY_CITY = "us_city";
-    public static final String KEY_IMG_AVATAR = "us_avatar";
+    public static final String KEY_AVATAR = "us_avatar";
     public static final String KEY_DESCRIPTION = "us_presentation";
     public static final String KEY_GENDER = "us_gender";
     public static final String KEY_SEXUAL_ORIENTATION = "us_sexual_orientation";
+    public static final String KEY_ETHNIE = "us_ethnic_group";
+    public static final String KEY_EYE_COLOR = "us_eye_color";
+    public static final String KEY_HAIR_COLOR = "us_hair_color";
+    public static final String KEY_HAIR_LENGTH = "us_hair_length";
     public static final String KEY_HOBBIES = "us_hobbies";
     public static final String KEY_MARITAL_STATUS = "us_marital_status";
+    public static final String KEY_SMOKE ="us_smoker";
+    public static final String KEY_SHAPE = "us_shape";
+    public static final String KEY_SPORTS = "us_sports";
+    public static final String KEY_PERSONALITY = "us_personality";
+
     public static final String KEY_HAS_KID = "us_has_kids";
-    private static final String US_AVATAR ="us_avatar";
+    public static final String KEY_BALANCE = "us_balance";
 
 
 
@@ -30,5 +40,6 @@ public class NodesNames {
 
     public static final String KEY_FS_USER_HOBBIE = "hobbies";
     public static final String KEY_HOBBIES_NAME = "ho_name";
+
 
 }
